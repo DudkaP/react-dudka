860c3036a07454b3b4623b8abb18985e67f20f7d
DudkaP <broxik666@gmail.com>
2022-04-23 11:15:33 +0200

    package.json + redux + react-redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
-
+import {Provider} from "react-redux";
+import {createStore} from "redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </React.StrictMode>
 
+function reducer(state='Hello', action) {
+    return state + action.payload;
+}
+
+let store = createStore(reducer);
+
+
+store.dispatch({type:'', payload:'!!!'})
+console.log(store.getState())
+
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
